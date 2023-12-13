c8f688fa2f503e4f81e137a60313d304a4283064
rocketstrong600 <rocketstrong600@gmail.com>
2023-12-13 18:46:48 +1000

    Settings implemented

--- filter/rastertodp.c
+++ filter/rastertodp.c
@@ -1,6 +1,24 @@
 #include <cups/cups.h>
 #include <cups/ppd.h>
 #include <cups/raster.h>
+#include "libdither.h"
+
+// settings struct
+
+typedef struct PrintSettings {
+  cups_bool_t InsertSheet;
+  cups_adv_t AdvanceMedia;
+  cups_cut_t CutMedia;
+  unsigned int AdvanceDistance;
+  unsigned int DitherMode;
+} PrintSettings;
+
+void ReadSettings(cups_page_header2_t *header, PrintSettings *settings) {
+  settings->AdvanceMedia = header->AdvanceMedia;
+  settings->CutMedia = header->CutMedia;
+  settings->AdvanceDistance = header->AdvanceDistance;
+  settings->DitherMode = header->cupsInteger[0];
+}
 
 void initPrinter() {
   char buf[] = {0x1b, 0x40};
@@ -12,28 +30,40 @@ void imageMode(uint16_t xsize, uint16_t ysize) {
   fwrite(buf, 1, sizeof(buf), stdout);
 }
 
+void feedLines(uint8_t amount) {
+  char buf[] = {0x1b, 0x64, amount};
+  fwrite(buf, 1, sizeof(buf), stdout);
+}
+
+void cutPaper() {
+  char buf[] = {0x1d, 0x6d};
+  fwrite(buf, 1, sizeof(buf), stdout);
+}
+
 uint16_t pixelToByte(uint16_t position) {;
   position = (position+7) & 0xFFFFFFF8;
   position = position >> 3;
   return position;
 }
 
+
 int main(int argc, char *argv[]) {
   cups_raster_t *ras = cupsRasterOpen(0, CUPS_RASTER_READ);
   cups_page_header2_t header;
   int page = 0;
   unsigned char *rasterLine = NULL;
-  
+  PrintSettings settings;
   while (cupsRasterReadHeader2(ras, &header)) {
     page++;
+    ReadSettings(&header, &settings);
     initPrinter();
-    fprintf(stdout, "PAGE: %i/%i\n", page, header.NumCopies);
-    fprintf(stdout, "BPP: %u\n", header.cupsBitsPerPixel);
-    fprintf(stdout, "BitsPerColour: %u\n", header.cupsBitsPerColor);
-    fprintf(stdout, "Width: %u\n", header.cupsWidth);
-    fprintf(stdout, "Height: %u\n", header.cupsHeight);
+    fprintf(stderr, "PAGE: %i/%i\n", page, header.NumCopies);
+    fprintf(stderr, "BPP: %u\n", header.cupsBitsPerPixel);
+    fprintf(stderr, "BitsPerColour: %u\n", header.cupsBitsPerColor);
+    fprintf(stderr, "Width: %u\n", header.cupsWidth);
+    fprintf(stderr, "Height: %u\n", header.cupsHeight);
     
-    rasterLine = calloc(header.cupsBytesPerLine, 1);
+    rasterLine = malloc(header.cupsBytesPerLine);
 
     for (unsigned int y = 0; y < header.cupsHeight; y++) {
       if (cupsRasterReadPixels(ras, rasterLine, header.cupsBytesPerLine) == 0) {
@@ -43,7 +73,7 @@ int main(int argc, char *argv[]) {
       imageMode(outSize, 1);
 
       unsigned char *outputLine = NULL;
-      outputLine = calloc(outSize, 1);
+      outputLine = malloc(outSize);
 
       for(unsigned int pixel = 0; pixel < header.cupsWidth; pixel++) {
         unsigned char pdata = rasterLine[pixel];
@@ -55,12 +85,34 @@ int main(int argc, char *argv[]) {
       }
       fwrite(outputLine, 1, outSize, stdout);
       free(outputLine);
+      outputLine = NULL;
     }
     
     free(rasterLine);
+    rasterLine = NULL;
+    
+    //advance every page
+    if (settings.AdvanceMedia == CUPS_ADVANCE_PAGE) {
+      feedLines(settings.AdvanceDistance);
+    }
+
+    //cut every page
+    if (settings.CutMedia == CUPS_CUT_PAGE) {
+      cutPaper();
+    }
     
   }
   cupsRasterClose(ras);
-  
+
+  //advance every job
+  if (settings.AdvanceMedia == CUPS_ADVANCE_JOB) {
+    feedLines(settings.AdvanceDistance)
+  }
+
+  //cut every job
+  if (settings.CutMedia == CUPS_CUT_JOB) {
+    cutPaper();
+  }
+
   return 0;
 }
